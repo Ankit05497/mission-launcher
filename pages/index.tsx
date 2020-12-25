09225ead5de12c014090a2a122372d274441a132
Ankit Gupta <gupta05497@gmail.com>
2020-12-25 22:13:34 +0530

    adds base structure to launcher project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NextPage } from 'next';
+import { Home } from '../components/home';
+
+const HomePage: NextPage = (_props) => {
+  return (
+    <Home />
+  );
+};
+
+export default HomePage;
